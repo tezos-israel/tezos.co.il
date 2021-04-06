ef2bdca74423927e1572f8cf3ef51b0758270816
Ameed Jadallah <ameed.jadallah@gmail.com>
2021-04-06 13:31:22 +0300

    feat(homepage): remove dangerouslySetInnerHTML function

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 function About({ title, content, image }) {
   return (
     <div className="sm:py-20 py-10 sm:px-0 px-7 bg-tezos-blue text-white">
@@ -9,10 +8,9 @@ function About({ title, content, image }) {
           {title}
         </h2>
         <div className="flex items-center">
-          <div
-            className="sm:text-xl text-md w-2/3 leading-normal"
-            dangerouslySetInnerHTML={{ __html: content }}
-          ></div>
+          <div className="sm:text-xl text-md w-2/3 leading-normal">
+            {content}
+          </div>
           <div className="text-right w-1/3">
             <img src={image} width="200" />
           </div>
