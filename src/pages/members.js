0144b77665a8ea259c2facdbf31455e9afb009ca
Aharon Lando <ahla85@gmail.com>
2021-04-12 06:44:51 +0300

    fix(team) center header without over-centering components

--- src/pages/members.js
+++ src/pages/members.js
@@ -4,8 +4,10 @@ import data from '../data/teamData.json';
 
 function Members() {
   return (
-    <div className="bg-tezos-dark flex flex-col items-center">
-      <p className="text-white text-5xl font-bold museo">Our Team</p>
+    <div className="bg-tezos-dark flex flex-col">
+      <p className="text-white text-5xl font-bold museo self-center">
+        Our Team
+      </p>
       <div className="grid lg:grid-cols-3 sm:grid-cols-1 justify-items-center space-x-8 space-y-4">
         {data.map((item, index) => {
           return (
