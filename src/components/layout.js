b6309954564a30fe696b6fb8bf049beb98ba7647
Chaim Lev-Ari <chiptus@gmail.com>
2021-05-25 14:01:59 +0300

    feat(blog): remove blog posts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './header';
