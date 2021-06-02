8f9c97fd5b9b13261febc82784abb5c6449fcbcb
Chaim Lev-Ari <chiptus@gmail.com>
2021-06-02 07:30:36 +0300

    feat(tags): add tag page

---
+++ src/components/blog-layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Layout from '../components/layout';
+
+export function BlogLayout({ children, title = 'Blog' }) {
+  return (
+    <Layout>
+      <div className="bg-gray-100">
+        <div className="max-w-7xl mx-auto py-9">
+          <h1 className="text-3xl text-center font-museo font-bold">{title}</h1>
+
+          {children}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+BlogLayout.propTypes = {
+  children: PropTypes.any,
+  title: PropTypes.string,
+};
