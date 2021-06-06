bee7282b957ed325ec6812305389b30165ca36f3
Chaim Lev-Ari <chiptus@gmail.com>
2021-06-06 11:51:11 +0300

    refactor(config): move rss config to another file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const rssFeedConfig = require('./src/gatsby-config/rss-feed');
+
 const siteUrl = 'https://tezos.co.il';
 
 module.exports = {
@@ -85,87 +87,6 @@ module.exports = {
         icon: `src/images/favicon-32x32.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        feeds: [
-          {
-            serialize: ({ query: { allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map((node) => {
-                const url = `${siteUrl}${node.fields.slug}`;
-                const author = node.frontmatter.authorFull;
-                const mediumHandle = `@${author.mediumHandle}`;
-
-                return {
-                  ...node.frontmatter,
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  author: `${author.name} (@${author.mediumHandle})`,
-                  url,
-                  guid: url,
-                  custom_elements: [
-                    {
-                      'content:encoded': `
-                      <p>This post was originally publish on our <a href="${url}">blog</a> by ${
-                        author.mediumHandle ? mediumHandle : author.name
-                      }</p>
-                      
-                      ${node.frontmatter.mediumContent || node.html}
-                      `,
-                    },
-                    { tags: node.frontmatter.tags.join(',') },
-                    { author: `${author.name} (@${author.mediumHandle})` },
-                    {
-                      featuredImage: `${siteUrl}/${node.frontmatter.featuredImage.childImageSharp.fixed.src}`,
-                    },
-                  ],
-                };
-              });
-            },
-            query: `
-              {
-              allMarkdownRemark(
-                filter: {frontmatter: {templateKey: {eq: "_blog-post"}}}
-                sort: {order: DESC, fields: [frontmatter___date]}
-              ) {
-                nodes {
-                  excerpt
-                  html
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                    date
-                    tags
-                    mediumContent
-                    authorFull {
-                      name
-                      email
-                      mediumHandle
-                    }
-                    featuredImage {
-                      childImageSharp {
-                        fixed {
-                          src
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            `,
-            output: '/rss.xml',
-            title: "Tezos Israel's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: '^/blog/',
-          },
-        ],
-      },
-    },
+    rssFeedConfig(siteUrl),
   ],
 };
