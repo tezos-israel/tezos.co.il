6a8d41b80221e1b2d0f88526028459e03739cbf4
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2021-07-12 17:28:29 +0300

    feat(contact): add contact form (#80)
    * feat(contact): add contact form
    
    * feat(contact): change from contact to follow

---
+++ src/components/contact-form.js
@@ -0,0 +1,105 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+import InputField from './shared/inputField';
+import Button from './shared/button';
+
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+const url =
+  'https://tezos.us7.list-manage.com/subscribe/post?u=006fde8ddcc4846cc107064ee&id=a34a3822b0';
+
+export function ContactUsForm() {
+  return (
+    <div className="sm:w-1/2 w-full">
+      <MailchimpSubscribe
+        url={url}
+        render={({ subscribe, status, message }) => (
+          <Form onSubmit={subscribe} status={status} message={message} />
+        )}
+      />
+    </div>
+  );
+}
+
+function Form({ onSubmit, status, message }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+
+  const isValid = name && email && email.includes('@');
+  const isLoading = status === 'sending';
+
+  return (
+    <form
+      className="flex flex-wrap justify-between flex-col space-y-4"
+      onSubmit={handleSubmit}
+    >
+      {status && <Status status={status} message={message} />}
+
+      <InputField
+        label="Name"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <InputField
+        label="Company"
+        id="company"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+      />
+      <InputField
+        label="Email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Button
+        disabled={isLoading || !isValid}
+        title="Send"
+        className="bg-tezos-blue text-white w-full sm:py-3 py-1 disabled:bg-gray-500 disabled:opacity-40"
+      />
+    </form>
+  );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !email || !email.includes('@')) {
+      return;
+    }
+
+    onSubmit({
+      EMAIL: email,
+      NAME: name,
+      COMPANY: company,
+    });
+  }
+}
+
+Form.propTypes = {
+  message: PropTypes.string,
+  onSubmit: PropTypes.func,
+  status: PropTypes.string,
+};
+
+function Status({ status, message }) {
+  if (status === 'sending') {
+    return <div className="">sending...</div>;
+  }
+
+  if (status === 'error') {
+    return <div className="" dangerouslySetInnerHTML={{ __html: message }} />;
+  }
+
+  if (status === 'success') {
+    return <div className="" dangerouslySetInnerHTML={{ __html: message }} />;
+  }
+
+  return null;
+}
+
+Status.propTypes = {
+  message: PropTypes.string,
+  status: PropTypes.oneOf(['sending', 'error', 'success']),
+};
--- src/components/shared/button.js
+++ src/components/shared/button.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-function Button({ title, className }) {
+function Button({ title, className, disabled }) {
   return (
     <button
+      disabled={disabled}
       className={classnames(
         'capitalize py-2 px-6 rounded-full text-sm focus:outline-none',
         className
@@ -16,8 +17,9 @@ function Button({ title, className }) {
 }
 
 Button.propTypes = {
-  title: PropTypes.string.isRequired,
   className: PropTypes.string,
+  disabled: PropTypes.bool,
+  title: PropTypes.string.isRequired,
 };
 
 export default Button;
