035ffdf887396b67c99e88ddf76a10c9ae65c8de
Aharon Lando <ahla85@gmail.com>
2021-07-20 16:31:42 +0300

    feat(featured): add featuredPost query

---
+++ src/components/featured.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Featured() {
+  return (
+    <div className="bg-gray-100">
+      <div className="max-w-7xl mx-auto py-9">
+        <h2 className="xl:text-xl text-lg font-museo px-4 mb-4 sm:text-left text-center capitalize">
+          Featured Post
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Featured;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,7 @@ import SEO from '../components/seo';
 import { graphql } from 'gatsby';
 
 import RecentPosts from '../components/recentPosts';
+import Featured from '../components/featured';
 
 function Blogs({
   data: {
@@ -25,12 +26,15 @@ function Blogs({
     }
   );
 
+  console.log(recentPosts[0]);
+
   return (
     <Layout>
       <SEO title="Blog" />
 
       {/* <MostPopular popularBlogs={data.popularBlogs} /> */}
 
+      <Featured />
       <RecentPosts posts={recentPosts} />
     </Layout>
   );
@@ -48,7 +52,7 @@ export default Blogs;
 
 export const pageQuery = graphql`
   query PostsTemplate {
-    allMarkdownRemark(
+    reacentPosts: allMarkdownRemark(
       filter: {
         frontmatter: {
           templateKey: { eq: "_blog-post" }
@@ -90,6 +94,52 @@ export const pageQuery = graphql`
               }
             }
           }
+        }
+      }
+    }
+    featuredPost: allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: { eq: "_blog-post" }
+          published: { eq: true }
+        }
+      }
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: 1
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            category
+            title
+            date
+            image: featuredImage {
+              childImageSharp {
+                gatsbyImageData(
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+            }
+            tags
+            author: authorFull {
+              name
+              email
+              image {
+                childImageSharp {
+                  gatsbyImageData(
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                  )
+                }
+              }
+            }
+          }
+          html
         }
       }
     }
