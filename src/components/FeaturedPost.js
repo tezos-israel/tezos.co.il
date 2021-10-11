1848688ee23bd8fd8504d52b6b4ea6e039975939
Chaim Lev-Ari <chiptus@gmail.com>
2021-10-11 19:30:36 +0300

    wip

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -5,10 +5,7 @@ import { Link } from 'gatsby';
 import { format } from 'date-fns';
 import Button from './shared/button';
 
-function Featured({ data }) {
-  const slug = data.fields.slug;
-  const frontmatter = data.frontmatter;
-  const author = frontmatter.author;
+function FeaturedPost({ url, title, author, image, date }) {
   return (
     <div className="bg-gray-100">
       <div className="md:max-w-7xl max-w-full mx-auto py-9">
@@ -19,15 +16,15 @@ function Featured({ data }) {
           <div className="flex md:flex-row flex-col space-x-2 sm:rounded-md rounded-none bg-gray-300 lg:p-8 p-4 lg:shadow-md shadow-sm">
             <div className="lg:h-56 md:h-56 h-24 max-w-full m-auto rounded-md overflow-hidden">
               <GatsbyImage
-                image={getImage(frontmatter.image)}
-                alt={frontmatter.title}
+                image={getImage(image)}
+                alt={title}
                 className="rounded-md margin-auto lg:w-full lg:h-full h-48"
               />
             </div>
             <div className="flex flex-col items-center space-y-4">
               <div className="text-center">
                 <h2 className="font-museo text-xl xl:w-3/4 lg:w-2/3 md:w-3/4 mx-auto my-4">
-                  {frontmatter.title}
+                  {title}
                 </h2>
               </div>
               <div className="w-16 h-16 rounded-full overflow-hidden mx-auto border-white border-5 shadow-lg">
@@ -36,10 +33,10 @@ function Featured({ data }) {
               <div className="text-sm mb-3">
                 <h4>{author.name}</h4>
                 <div className="text-black text-opacity-50">
-                  {format(new Date(frontmatter.date), 'yyyy-MM-dd')}
+                  {format(new Date(date), 'yyyy-MM-dd')}
                 </div>
               </div>
-              <Link to={slug} className="self-center my-4">
+              <Link to={url} className="self-center my-4">
                 <Button
                   title="Go To Post"
                   className="bg-tezos-blue text-white"
@@ -53,21 +50,15 @@ function Featured({ data }) {
   );
 }
 
-Featured.propTypes = {
-  data: PropTypes.shape({
-    frontmatter: PropTypes.shape({
-      title: PropTypes.string,
-      author: PropTypes.shape({
-        name: PropTypes.string,
-        image: PropTypes.any,
-      }),
-      image: PropTypes.any,
-      date: PropTypes.string,
-    }),
-    fields: PropTypes.shape({
-      slug: PropTypes.string,
-    }),
+FeaturedPost.propTypes = {
+  author: PropTypes.shape({
+    image: PropTypes.object.isRequired,
+    name: PropTypes.string,
   }).isRequired,
+  date: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
 
-export default Featured;
+export default FeaturedPost;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,13 +5,18 @@ import SEO from '../components/seo';
 import { graphql } from 'gatsby';
 
 import RecentPosts from '../components/recentPosts';
-import Featured from '../components/featured';
+import FeaturedPost from '../components/FeaturedPost';
 
 function Blogs({
   data: {
     recentPosts: { edges },
+    featuredPost: featuredPostQueryResult,
   },
 }) {
+  if (featuredPostQueryResult.edges.length === 0) {
+    throw new Error('must define a featured post');
+  }
+
   const recentPosts = edges.map(
     ({
       node: {
@@ -25,7 +30,8 @@ function Blogs({
       };
     }
   );
-  const featuredPost = edges[0].node;
+
+  const featuredPost = featuredPostQueryResult.edges[0];
 
   return (
     <Layout>
@@ -33,7 +39,13 @@ function Blogs({
 
       {/* <MostPopular popularBlogs={data.popularBlogs} /> */}
 
-      <Featured data={featuredPost} />
+      <FeaturedPost
+        url={featuredPost.fields.slug}
+        title={featuredPost.frontmatter.title}
+        author={featuredPost.frontmatter.author}
+        image={featuredPost.frontmatter.image}
+        date={featuredPost.frontmatter.date}
+      />
       <RecentPosts posts={recentPosts} />
     </Layout>
   );
@@ -44,6 +56,15 @@ Blogs.propTypes = {
     recentPosts: PropTypes.shape({
       edges: PropTypes.array,
     }),
+    featuredPost: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          frontmatter: PropTypes.shape({
+            title: PropTypes.string,
+          }),
+        })
+      ),
+    }),
   }).isRequired,
 };
 
@@ -51,6 +72,51 @@ export default Blogs;
 
 export const pageQuery = graphql`
   query PostsTemplate {
+    featuredPost: allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: { eq: "_blog-post" }
+          featuredPost: { eq: true }
+        }
+      }
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: 1
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            category
+            title
+            date
+            image: featuredImage {
+              childImageSharp {
+                gatsbyImageData(
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+            }
+            tags
+            author: authorFull {
+              name
+              email
+              image {
+                childImageSharp {
+                  gatsbyImageData(
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                  )
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     recentPosts: allMarkdownRemark(
       filter: {
         frontmatter: {
